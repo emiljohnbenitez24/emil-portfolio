7c0a4d9eae49c2fb186f8f187fb96eee208a3285
Emil John <kriempots@gmail.com>
2020-02-13 13:36:38 +0800

    Final Commit

--- src/components/projects.js
+++ src/components/projects.js
@@ -17,10 +17,11 @@ const Projects = () => {
     min-height:70vh;
     .hovereffect {
         width: 100%;
-        height: 100%;
+        min-height:40vh;
         overflow: hidden;
         position: relative;
         text-align: center;
+        -webkit-transition: all 0.5s ease;
         cursor: default;
       }
       
@@ -35,63 +36,53 @@ const Projects = () => {
       
       .hovereffect img {
         position: relative;
-        -webkit-transition: all 0.4s ease-in;
+        -webkit-transition: all 0.5s ease;
         transition: all 0.4s ease-in;
       }
       
-      .hovereffect:hover img {
-        filter: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg"><filter id="filter"><feColorMatrix type="matrix" color-interpolation-filters="sRGB" values="0.2126 0.7152 0.0722 0 0 0.2126 0.7152 0.0722 0 0 0.2126 0.7152 0.0722 0 0 0 0 0 1 0" /><feGaussianBlur stdDeviation="3" /></filter></svg>#filter');
-        filter: grayscale(1) blur(2px);
-        -webkit-filter: grayscale(1) blur(3px);
-        -webkit-transform: scale(1.1);
-        -ms-transform: scale(1.1);
-        transform: scale(1.1);
+      .hovereffect:hover {
+        background-color:rgb(11, 2, 34);
+        opacity:#191970;
+        -webkit-transition: all 0.5s ease;
+      }
+
+      .hovereffect:hover img{
+        opacity:0;
       }
       
       .hovereffect h2 {
-        text-transform: uppercase;
         text-align: center;
         align-items:center;
+        margin-top:20px;
         justify-content:center;
         position: relative;
         font-size: 17px;
         padding: 10px;
-        background: rgba(0, 0, 0, 0.6);
       }
       
       .hovereffect a.info {
         display: inline-block;
-        text-decoration: none;
-        border-color:orange !important;
+        text-decoration:none;
         padding: 5px;
-        border: 1px solid #fff;
-        margin: 50px 0 0 0;
+        border: 2px solid #8A2BE2;
+        margin: 50px 10px 0 10px;
         background: rgba(0, 0, 0, 0.6);
       }
       
       .hovereffect a.info:hover {
-        box-shadow: 0 0 5px #fff;
-        color:orange !important;
+        background-color:#8A2BE2;
+        color:white !important;
       }
       
       .hovereffect a.info, .hovereffect h2 {
-        -webkit-transform: scale(0.6);
-        -ms-transform: scale(0.6);
-        transform: scale(0.6);
         -webkit-transition: all 0.4s ease-in;
         transition: all 0.4s ease-in;
         opacity: 0;
-        filter: alpha(opacity=0);
         color: #fff;
-        text-transform: uppercase;
       }
       
       .hovereffect:hover a.info, .hovereffect:hover h2 {
         opacity: 1;
-        filter: alpha(opacity=100);
-        -webkit-transform: scale(0.7);
-        -ms-transform: scale(0.7);
-        transform: scale(0.7);
       }
         `
 
@@ -105,7 +96,7 @@ const Projects = () => {
           <Col>
             <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
               <div class="hovereffect" style={{ marginBottom: '5vh' }}>
-                <Image width={200} style={{ padding: '5vh' }} height={225} class="img-responsive" src={phone} />
+                <Image width={200} height={225} class="img-responsive" src={phone} />
                 <div class="overlay">
                   <h2>Shopping Cart <p >Using Pure React</p></h2>
                   <a class="info" href="https://ej-react-shoppingcart.herokuapp.com/">View Project</a>
@@ -119,7 +110,7 @@ const Projects = () => {
               <div class="hovereffect" style={{ marginBottom: '5vh' }}>
                 <Image width={200} height={210} class="img-responsive" src={flower} />
                 <div class="overlay">
-                  <h2>FlowerShop WebApp            <p >Using React w/Redux</p></h2>
+                  <h2>FlowerShop WebApp <p >Using React w/Redux</p></h2>
                   <a class="info" href="https://ejflowershop.herokuapp.com">View Project</a>
                   <a class="info" href="https://github.com/emiljohnbenitez24/emil-flowershop">View Code</a>
                 </div>
